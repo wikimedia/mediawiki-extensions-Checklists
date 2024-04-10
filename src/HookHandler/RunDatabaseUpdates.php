82ee48c832d918d48013a924ca78c8c870c3a95f
hslater <hamish.slater@hallowelt.com>
2024-04-10 21:21:14 +1200

    Convert to abstract schema
    ERM36733
    
    [REL1_39, REL1_39-1.0.x, master]
    
    Change-Id: Ied2bde0254444fcf17fdf1396ba4399276af3282
    (cherry picked from commit 7f678bbd3fa6d593c83f153988b4a816e6506443)

--- src/HookHandler/RunDatabaseUpdates.php
+++ src/HookHandler/RunDatabaseUpdates.php
@@ -10,9 +10,12 @@ class RunDatabaseUpdates implements LoadExtensionSchemaUpdatesHook {
 	 * @inheritDoc
 	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
+		$dbType = $updater->getDB()->getType();
+		$dir = dirname( __DIR__, 2 );
+
 		$updater->addExtensionTable(
 			'checklist_items',
-			__DIR__ . '/../../db/checklist_items.sql'
+			"$dir/db/$dbType/checklist_items.sql"
 		);
 	}
 }
